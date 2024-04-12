6d592fcc854b3ae960d23abdf4257d732b6196bf
hojinlee <infohojin@gmail.com>
2024-04-13 00:31:47 +0900

    first

---
+++ src/JinyWidgetServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+namespace Jiny\Widgets;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\View\Compilers\BladeCompiler;
+use Livewire\Livewire;
+
+use Illuminate\Routing\Router;
+
+class JinyWidgetServiceProvider extends ServiceProvider
+{
+    private $package = "jiny-widgets";
+    public function boot()
+    {
+        // 모듈: 라우트 설정
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', $this->package);
+
+        // 데이터베이스
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+        // 설정파일 복사
+        $this->publishes([
+            __DIR__.'/../config/setting.php' => config_path('jiny/widgets/setting.php'),
+        ]);
+
+
+    }
+
+    public function register()
+    {
+        /* 라이브와이어 컴포넌트 등록 */
+        $this->app->afterResolving(BladeCompiler::class, function () {
+       
+            
+        });
+
+    }
+
+}
